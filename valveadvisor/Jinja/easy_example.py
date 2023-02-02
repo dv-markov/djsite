a168d78bb84c3fc605d8acba1a6d7c9cd86d3248
dv-markov <dmitry.v.markov@gmail.com>
2023-02-02 22:47:16 +0300

    msk-home

--- valveadvisor/Jinja/easy_example.py
+++ valveadvisor/Jinja/easy_example.py
@@ -20,3 +20,9 @@ tm = Template("Мне {{ p.age }} лет и меня зовут {{ p.name }} �
 msg = tm.render(p=per)
 
 print(msg)
+
+
+pp = {'name': 'Ivan', 'age': 34}
+tm = Template("А меня зовут {{ p.name }}, мне {{ p.age }} 😎")
+msg = tm.render(p=pp)
+print(msg)
