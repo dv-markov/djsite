4e2f40a6dd7c22ddd851bf5ec4c6fa67b7491014
dv-markov <dmitry.v.markov@gmail.com>
2023-02-04 14:54:45 +0300

    msk-home

--- valveadvisor/valveadvisor/settings.py
+++ valveadvisor/valveadvisor/settings.py
@@ -119,6 +119,13 @@ STATIC_URL = 'static/'
 # новая запись из урока 7
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
+# print('******************', BASE_DIR, '*********************')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "women/",
+#     BASE_DIR / "women/static/",
+#     "E:/_ARCHIVE/YADISK/Python/Projects/djsite/valveadvisor/women/static/"
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
